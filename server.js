6a6e37b592293bfc197f4ffda282a105fd9489d9
Capi Etheriel <barraponto@gmail.com>
2016-07-21 15:32:18 -0300

    introducing POST /items

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
-var Storage = require('./storage.js');
-var storage = new Storage();
+var parsers = require('body-parser');
+var jsonParser = parsers.json();
 
+var Storage = require('./storage.js');
+
+var storage = new Storage();
 storage.add('Broad beans');
 storage.add('Tomatoes');
 storage.add('Peppers');
@@ -11,6 +14,12 @@ app.use(express.static('public'));
 
 app.get('/items', function(request, response){
   response.json(storage.items);
+});
+
+app.post('/items', jsonParser, function(request, response){
+  if (!request.body) { return response.sendStatus(400); }
+  var item = storage.add(request.body.name);
+  response.status(201).json(item);
 });
 
 app.listen(process.env.PORT || 3000);
