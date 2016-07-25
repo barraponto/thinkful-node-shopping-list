85c304e399d8f448ad7958397e5b603c6576e4bf
Capi Etheriel <barraponto@gmail.com>
2016-07-24 21:29:31 -0300

    add tests

---
+++ test/test-server.js
@@ -0,0 +1,53 @@
+var chai = require('chai');
+var http = require('chai-http');
+var server = require('../server.js');
+
+var should = chai.should();
+var app = server.app;
+var storage = server.storage;
+
+chai.use(http);
+
+
+describe('Shopping List', function(){
+  it('should list items on get', function(done){
+    chai.request(app).get('/items').end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('array');
+      res.body.should.have.length(storage.items.length);
+      res.body[0].should.have.property('name');
+      res.body[0].should.have.property('id');
+      res.body[0].id.should.be.a('number');
+      res.body[0].name.should.be.a('string');
+      res.body[0].name.should.equal('Broad beans');
+      res.body[1].name.should.equal('Tomatoes');
+      res.body[2].name.should.equal('Peppers');
+      done();
+    });
+  });
+  it('should add items on post', function(done){
+    chai.request(app).post('/items').send({'name': 'Kale'}).end(function(err, res){
+      should.equal(err, null);
+      res.should.have.status(201);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('name');
+      res.body.should.have.property('id');
+      res.body.id.should.be.a('number');
+      res.body.name.should.be.a('string');
+      res.body.name.should.equal('Kale');
+      storage.items.should.be.a('array');
+      storage.items.should.have.length(4);
+      storage.items[3].should.be.a('object');
+      storage.items[3].should.have.property('id');
+      storage.items[3].should.have.property('name');
+      storage.items[3].id.should.be.a('number');
+      storage.items[3].name.should.be.a('string');
+      storage.items[3].name.should.equal('Kale');
+      done();
+    });
+  });
+  it('should edit item on put');
+  it('should delete item on delete');
+});
